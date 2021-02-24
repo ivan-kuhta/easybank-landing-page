732f47b9e33cc8108bec0dd915ab591a9607840b
ivan-kuhta <filmplex22342@gmail.com>
2021-02-24 14:36:04 +0200

    feat: add mobile menu && animation mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', () => {
+  if(header.classList.contains('open')) {
+    body.classList.remove('noscroll')
+    header.classList.remove('open');
+    fadeElems.forEach(elem => {
+      elem.classList.remove('fade-in');
+      elem.classList.add('fade-out');
+    })
+  }else {
+    body.classList.add('noscroll')
+    header.classList.add('open');
+    fadeElems.forEach(elem => {
+      elem.classList.remove('fade-out');
+      elem.classList.add('fade-in');
+    })
+  }
+})
